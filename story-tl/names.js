c951245abaa4c40ee6e3eb76afe5a7003077c683
Jet Rico <jetriconew@gmail.com>
2018-09-09 11:06:19 +0800

    story-tl name extractor

---
+++ story-tl/names.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const fs = require('fs')
+const glob = require('glob')
+const Promise = require('bluebird')
+
+let files = glob.sync('output/*.json')
+let allNames = []
+let uniqueNames = []
+
+Promise.each(files, sourceFile => {
+  if (sourceFile.substring(sourceFile.length - 10) == 'names.json') return
+  return Promise.resolve({ sourceFile })
+    .then(require('./modules/readFile'))
+    .then(data => {
+      let messages = JSON.parse(data.contents).messages
+      let names = messages.map(message => message.ch)
+      names.forEach(name => {
+        if (name && (uniqueNames.indexOf(name) === -1)) {
+          allNames.push({
+            zh: name,
+            tl: ''
+          })
+          uniqueNames.push(name)
+        }
+      })
+    })
+    .catch(err => {
+      console.log('ERR', sourceFile);
+    })
+})
+.then(() => {
+  fs.writeFileSync(path.join(__dirname, 'output', 'names.json'), JSON.stringify({ names: allNames }, null, 2))
+})
