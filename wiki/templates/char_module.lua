ec150a1a8445f4f01a9c33071c41ed3ec8a2bd14
Jet Rico <jetriconew@gmail.com>
2019-03-19 15:43:11 +0800

    char_module.lua

--- wiki/templates/char_module.lua
+++ wiki/templates/char_module.lua
@@ -1,53 +1,71 @@
 return {
-  id = "204",
-  num = "R204",
+  id = "{{id}}",
+  num = "{{num}}",
   name = {
-    en = "Platinum",
-    cn = "白金",
-    jp = ""
+    en = "{{name_en}}",
+    cn = "{{name_cn}}",
+    jp = "{{name_jp}}",
+    kr = "{{name_kr}}",
   },
-  description = "Priority attack air unit",
-  team = 5,
-  position = "Ranged",
+  background = "{{background}}",
+  team = {{team}},
+  position = "{{position}}",
   role = {
-    "DPS"
+    "{{roles}}"
   },
-  faction = "Kazimierz",
-  stars = 4,
-  class = "Sniper",
-  item = {
-    usage = "Platinum is about to provide you with the perfect boost, so please settle this clerk.",
-    description = "The Ministry of Personnel has already contacted the unstoppable Platinum, and the other party seems to be interested in reaching some cooperation with you.",
-    obtain = "Recruitment search"
-  },
+  faction = "{{faction}}",
+  stars = {{stars}},
+  class = "{{class}}",
   ranks = {
-    base = ,
-    elite1 = ,
-    elite2 = 
+    base = {{rank_base}},
+    elite1 = {{rank_elite1}},
+    elite2 = {{rank_elite2}},
   },
   skills = {
-
+    {{skill1}},
+    {{skill2}},
+    {{skill3}},
   },
   talents = {
-
+    {{talents}},
   },
   potential = {
-
+    {{potential}},
   },
   trust = {
-
+    {{trust}},
   },
   profile = {
-    realname = "Texas",
-	gender = "female",
-    job = "Escort",
-    specialization = "Driving, Swordsmanship, Fighting",
-    race = "Texas Wolf",
-    raceLink = "https://en.wikipedia.org/wiki/Texas_wolf",
-    illustrator = "Skade",
-    voice = "",
+    base = {
+      realname = "{{realname}}",
+      codename = "{{codename}}",
+      gender = "{{gender}}",
+      combatexp = "{{combatexp}}",
+      origin = "{{origin}}",
+      birthday = "{{birthday}}",
+      race = "{{race}}",
+      height = "{{height}}",
+      weight = "{{weight}}",
+      oripathy = "{{oripathy}}",
+    },
+    extra = {
+      
+    },
+    physical = {
+      strength = "{{strength}}",
+      mobility = "{{mobility}}",
+      endurance = "{{endurance}}",
+      tactic = "{{tactic}}",
+      skill = "{{skill}}",
+      originium = "{{originium}}",
+    },
+    diagnosis = {{diagnosis}},
+    meta = {
+      illustrator = "{{illustrator}}",
+      voice = "{{voice}}",
+    },
   },
   quotes = {
-
+    {{quotes}}
   }
 }
