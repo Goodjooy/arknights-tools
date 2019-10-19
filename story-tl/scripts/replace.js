fc6cdbd6a2bc603c2db9416d56abe736fbfe620d
Jet Rico <jetriconew@gmail.com>
2019-10-20 02:33:24 +0800

    [story-tl] SW01

--- story-tl/scripts/replace.js
+++ story-tl/scripts/replace.js
@@ -5,6 +5,21 @@
   const path = require('path')
   const config = require('config')
 
-  // use json
+  let levelName = process.argv[2]
+  let translationsName = process.argv[3]
 
+  let levelData = fs.readFileSync('./assets/txt/' + levelName + '.txt', {encoding:'utf8'})
+  let names = JSON.parse(fs.readFileSync('./assets/json/Names.json'))
+  let translations = JSON.parse(fs.readFileSync('./assets/json/' + translationsName + '.json'))
+
+  translations.messages.forEach(tlObject => {
+    levelData = levelData.replace(tlObject.zh, tlObject.en)
+  })
+
+  names.messages.forEach(nameObject => {
+    levelData = levelData.replace(new RegExp(nameObject.zh, 'g'), nameObject.en)
+  })
+
+  fs.writeFileSync('./assets/tld/' + levelName + '.txt', levelData)
+  
 })()
